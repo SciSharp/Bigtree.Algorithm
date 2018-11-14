bc750edaac268d5cf16489a04609ef80f7c96c47
haiping008 <haiping008@gmail.com>
2018-11-14 17:23:52 -0600

    work with MatplotlibCS

--- Bigtree.Algorithm.UnitTest/LinearRegression/ModelTest.cs
+++ Bigtree.Algorithm.UnitTest/LinearRegression/ModelTest.cs
@@ -1,7 +1,10 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using MatplotlibCS;
+using MatplotlibCS.PlotItems;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
 using NumSharp;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 
 namespace Bigtree.Algorithm.UnitTest.LinearRegression
@@ -13,8 +16,50 @@ namespace Bigtree.Algorithm.UnitTest.LinearRegression
         public void Regression()
         {
             var np = new NumPy<double>();
+
             var data_x = np.linspace(1, 10, 100);
-            var data_y = np.sin(data_x);// + 0.1 * np.power;
+            var data_y = np.sin(data_x) + 0.1 * np.power(data_x, 2) + 0.5 * np.random.randn(100, 1);
+            data_x /= np.max(data_x)[0];
+
+            Plot(data_x, data_y);
+        }
+
+        private void Plot(NDArray<double> x, NDArray<double> y)
+        {
+            var matplotlibCs = new MatplotlibCS.MatplotlibCS("python", @"D:\Projects\MatplotlibCS\MatplotlibCS\Python\matplotlib_cs.py");
+
+            var items = new List<PlotItem>();
+            for(int i = 0; i < x.Size; i++)
+            {
+                items.Add(new Point2D($"P{i}", x[i], y[i])
+                {
+                    MarkerFaceColor = Color.Black,
+                    MarkerSize = 3
+                });
+            }
+
+            var figure = new Figure(1, 1)
+            {
+                FileName = "regression.png",
+                OnlySaveImage = true,
+                DPI = 150,
+                Subplots =
+                {
+                    new Axes(1, "X axis", "Y axis")
+                    {
+                        Title = "Regression Test",
+                        Grid = new Grid()
+                        {
+                            XLim = new double[] {0, 1.2},
+                            YLim = new double[] {-2, 12}
+                        },
+                        PlotItems = items
+                    }
+                }
+            };
+
+            var t = matplotlibCs.BuildFigure(figure);
+            t.Wait();
         }
     }
 }

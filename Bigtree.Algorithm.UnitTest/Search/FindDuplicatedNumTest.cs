e1e963c6d0d60850f3a96eb2afa1ee2ae4a5e6db
Oceania2018 <haiping008@gmail.com>
2018-12-06 07:42:31 -0600

    FindDuplicatedNum

---
+++ Bigtree.Algorithm.UnitTest/Search/FindDuplicatedNumTest.cs
@@ -0,0 +1,98 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NumSharp.Core;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Bigtree.Algorithm.UnitTest.Search
+{
+    [TestClass]
+    public class FindDuplicatedNumTest
+    {
+        private NumPy np;
+        private NDArray nd;
+        private int repeated;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            repeated = -1;
+            np = new NumPy();
+            nd = np.arange(10);
+            nd[8] = 5;
+            np.random.shuffle(nd);
+        }
+
+        [TestMethod]
+        public void FindDuplicatedNumber()
+        {
+            repeated = Method1(nd.int32);
+
+            repeated = Method2(nd.int32);
+
+            repeated = Method4(nd.int32);
+        }
+
+        private int Method1(int[] a)
+        {
+            int i;
+            int end = nd.size - 1;
+
+            for (i = 0; i <= end; i++)
+                a[end] += a[i];
+            a[end] -= i * (i - 1) / 2;
+
+            return a[end];
+        }
+
+        private int Method2(int[] array)
+        {
+            int index = 0;
+            while (true)
+            {
+                if (array[index] < 0)
+                    break;
+                array[index] *= -1;
+                index = array[index] * (-1);
+            }
+
+            return -array[index];
+        }
+
+        [TestMethod]
+        public void SLowFast()
+        {
+            int[] array = nd.int32;
+            int slow = 0;
+            int fast = 0;
+
+            while (true)
+            {
+                slow = array[slow];
+                fast = array[array[fast]];
+                if (slow == fast)
+                    break;
+            }
+
+            fast = 0;
+            while (true)
+            {
+                slow = array[slow];
+                fast = array[fast];
+                if (slow == fast)
+                    break;
+            }
+        }
+
+        private int Method4(int[] array)
+        {
+            int result = 0;
+            for (int i = 1; i <= nd.size - 1; i++)
+                result ^= i;
+            for (int i = 0; i <= nd.size - 1; i++)
+                result ^= array[i];
+
+            return result;
+        }
+    }
+}
